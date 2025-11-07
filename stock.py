495fb8de3657c75dc653467477fda9c1ac316dd3
HewaOrang <hewaorang@gmail.com>
2025-11-07 08:01:05 +0000

    creating script

---
+++ stock.py
@@ -0,0 +1,19 @@
+
+
+# Dates and times.
+import datetime as dt
+
+# Yahoo Finance data.
+import yfinance as yf
+
+# Get data.
+df = yf.download(['MSFT', 'AAPL', 'GOOG'], period='5d', interval='1h')
+
+# Current date and time.
+now =  dt.datetime.now()
+
+# File name.
+filename= "../data/" + now.strftime("%Y%m%d-%H%M%S") + '.csv'
+
+# Save data as csv.
+df.to_csv(filename)
