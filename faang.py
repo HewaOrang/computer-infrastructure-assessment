932ec0029d4309b9054e6e55df6809bf7139b1b8
HewaOrang <hewaorang@gmail.com>
2025-12-09 16:13:08 +0000

    recreating github actions workflow

--- faang.py
+++ faang.py
@@ -14,22 +14,17 @@ df = yf.download(['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG',], period='5d', interva
 
 # Current date and time.
 now =  dt.datetime.now()
-timestamp = now.strftime("%Y%m%d-%H%M%S")
 
 # File name.
-filename= "/data/" + timestamp + '.csv'
-# Ensure 'data' directory exists
-os.makedirs("data", exist_ok=True)
-
-# File name.
-filename = os.path.join("data", now.strftime("%Y%m%d-%H%M%S") + '.csv')
+filename= "./data/" + now.strftime("%Y%m%d-%H%M%S") + '.csv'
 
 # Save data as csv.
 df.to_csv(filename)
 
 # Create new figure and axis.
-# Use the same timestamp for the plot title.
-ax.set_title(f'Plot date: {timestamp}')
+fig, ax = plt.subplots()
+
+# Plot all closing pieces.
 df['Close'].plot(ax=ax)
 
 # Current date and time.
@@ -38,16 +33,13 @@ ax.set_title(f'Plot date: {now.strftime("%Y%m%d-%H%M%S")}')
 
 # Add axis labels.
 ax.set_xlabel("Date")
-# File name.
-filename= "/plots/" + timestamp + '.png'
+ax.set_ylabel("Close Price")
+
 # Add legend.
 ax.legend(loc='upper right')
 
-# Ensure 'plots' directory exists
-os.makedirs("plots", exist_ok=True)
-
 # File name.
-filename = os.path.join("plots", now.strftime("%Y%m%d-%H%M%S") + '.png')
+filename= "./plots/" + now.strftime("%Y%m%d-%H%M%S") + '.png'
 
 # Save figure.
 fig.savefig(filename, dpi=300)
